738e18190bccb880b2f6e3f63314731117c081e5
PeaceAntoHim <stefanusfranssebastian@gmail.com>
2023-12-26 17:41:06 +0700

    fix: fixing issue dependency and for deployment

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,21 +1,21 @@
-import type { AppProps } from 'next/app';
-import { ChakraProvider } from '@chakra-ui/react';
-import Router from 'next/router';
-import { useEffect } from 'react';
-import NProgress from 'nprogress';
-import Head from 'next/head';
+import type { AppProps } from "next/app";
+import { ChakraProvider } from "@chakra-ui/react";
+import Router from "next/router";
+import { useEffect } from "react";
+import NProgress from "nprogress";
+import Head from "next/head";
 
 export default function App({ Component, pageProps }: AppProps) {
   useEffect(() => {
-    Router.events.on('routeChangeStart', () => {
+    Router.events.on("routeChangeStart", () => {
       NProgress.start();
     });
 
-    Router.events.on('routeChangeComplete', () => {
+    Router.events.on("routeChangeComplete", () => {
       NProgress.done(false);
     });
 
-    Router.events.on('routeChangeError', () => {
+    Router.events.on("routeChangeError", () => {
       NProgress.done(false);
     });
   }, []);
