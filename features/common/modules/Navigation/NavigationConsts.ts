81c6ee2759c3a5a8fe95622574527ab8dbd017ac
PeaceAntoHim <stefanusfranssebastian@gmail.com>
2023-12-26 17:03:09 +0700

    feat: adding feature login

--- features/common/modules/Navigation/NavigationConsts.ts
+++ features/common/modules/Navigation/NavigationConsts.ts
@@ -1,5 +1,6 @@
-import { HiCube, HiNewspaper, HiPhoneArrowUpRight } from 'react-icons/hi2';
-import { IconType } from 'react-icons/lib';
+import { HiCube, HiNewspaper, HiPhoneArrowUpRight } from "react-icons/hi2";
+import { IconType } from "react-icons/lib";
+import { TbLogin } from "react-icons/tb";
 
 export type navigationLink = {
   title: string;
@@ -8,11 +9,12 @@ export type navigationLink = {
 };
 
 export const navigationLinks: Array<navigationLink> = [
-  { title: 'Properties', link: '/properties', icon: HiCube },
-  { title: 'Contact', link: '/contact', icon: HiNewspaper },
+  { title: "Properties", link: "/properties", icon: HiCube },
+  { title: "Contact", link: "/contact", icon: HiNewspaper },
   {
-    title: '(800) 555 555',
-    link: 'tel:(800) 555 555',
+    title: "(800) 555 555",
+    link: "tel:(800) 555 555",
     icon: HiPhoneArrowUpRight,
   },
+  { title: "Login", link: "/login", icon: TbLogin },
 ];
