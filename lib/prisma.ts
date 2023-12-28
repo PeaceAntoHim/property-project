2f200a84db937906bb403bcd6633da2c25f64fc5
PeaceAntoHim <stefanusfranssebastian@gmail.com>
2023-12-29 00:27:15 +0700

    fix: fixing err for type

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,11 +1,16 @@
-import { PrismaClient } from '@prisma/client';
+import { PrismaClient } from "@prisma/client";
+
+// Declare global object with prisma property
+declare global {
+  var prisma: PrismaClient | undefined;
+}
 
 let prisma: PrismaClient;
 
-
-if (process.env.NODE_ENV === 'production') {
+if (process.env.NODE_ENV === "production") {
   prisma = new PrismaClient();
 } else {
+  // Use global object if it exists, otherwise create and assign
   if (!global.prisma) {
     global.prisma = new PrismaClient();
   }
--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -4,20 +4,20 @@ import { PrismaAdapter } from '@next-auth/prisma-adapter'
 import GitHubProvider from 'next-auth/providers/github'
 import prisma from '../../../lib/prisma';
 
-const authHandler: NextApiHandler = (req, res) => NextAuth(req, res, options);
-export default authHandler;
-
 type GitHubProviderT = {
   clientId: string;
   clientSecret: string;
 }
 
+const authHandler: NextApiHandler = (req, res) => NextAuth(req, res, options);
+export default authHandler;
+
 const options = {
   providers: [
     GitHubProvider({
-      clientId: process.env.GITHUB_ID,
-      clientSecret: process.env.GITHUB_SECRET,
-    }),
+      clientId: process.env.GITHUB_ID as string,
+      clientSecret: process.env.GITHUB_SECRET as string,
+    } as GitHubProviderT),
   ],
   adapter: PrismaAdapter(prisma),
   secret: process.env.SECRET,
