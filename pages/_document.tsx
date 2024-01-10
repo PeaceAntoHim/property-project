d83ac4b4c018c7bb94b41601067913233f5eda0b
PeaceAntoHim <stefanusfranssebastian@gmail.com>
2024-01-10 23:57:04 +0700

    feat: add feature speed insight and web analythic, fix issue dependency

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,4 +1,6 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
+import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 export default function Document() {
   return (
@@ -7,7 +9,9 @@ export default function Document() {
       <body>
         <Main />
         <NextScript />
+        <Analytics />
+        <SpeedInsights />
       </body>
     </Html>
-  )
+  );
 }
