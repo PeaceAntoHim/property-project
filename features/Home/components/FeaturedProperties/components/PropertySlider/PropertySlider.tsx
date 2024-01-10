44ffeef731c02bb880bc9bea4833c7fbf3e2c978
PeaceAntoHim <stefanusfranssebastian@gmail.com>
2024-01-11 00:13:35 +0700

    fix: lib not used

--- features/Home/components/FeaturedProperties/components/PropertySlider/PropertySlider.tsx
+++ features/Home/components/FeaturedProperties/components/PropertySlider/PropertySlider.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
-import { Autoplay, Pagination } from "swiper";
+// import { Autoplay, Pagination } from "swiper";
 
 import "swiper/css";
 import "swiper/css/navigation";
