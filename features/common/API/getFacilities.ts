116ace5636d44df52454c5d68e0a78c9574097fd
PeaceAntoHim <stefanusfranssebastian@gmail.com>
2024-01-19 01:00:58 +0700

    fix: api integration

--- features/common/API/getFacilities.ts
+++ features/common/API/getFacilities.ts
@@ -1,7 +1,6 @@
 import { axiosOwnServer } from "@/lib/axios";
 
 export const getFacilities = async () => {
-  // const { data } = await axiosOwnServer.get("/facilities");
-  const data = [{}];
+  const { data } = await axiosOwnServer.get("/facilities");
   return data;
 };
