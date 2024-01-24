d435f4cdd6059fe5b011e7fbc1d86db235f22467
PeaceAntoHim <stefanusfranssebastian@gmail.com>
2024-01-25 00:22:58 +0700

    fix: deleted all meets team just add one

--- features/Home/components/MeetTheTeam/agentConst.ts
+++ features/Home/components/MeetTheTeam/agentConst.ts
@@ -6,25 +6,25 @@ export const agents = [
     description:
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam a velit fermentum, ultrices ligula sed, tincidunt est. Nullam vulputate sollicitudin nulla non pulvinar. In eget imperdiet magna.",
   },
-  {
-    name: "Levon Tomson",
-    title: "Real estate agent",
-    image: "./agents/agent-3.jpeg",
-    description:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam a velit fermentum, ultrices ligula sed, tincidunt est. Nullam vulputate sollicitudin nulla non pulvinar. In eget imperdiet magna.",
-  },
-  {
-    name: "Linnet Cubin",
-    title: "Real estate agent",
-    image: "./agents/agent-2.jpeg",
-    description:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam a velit fermentum, ultrices ligula sed, tincidunt est. Nullam vulputate sollicitudin nulla non pulvinar. In eget imperdiet magna.",
-  },
-  {
-    name: "Roddy Epsley",
-    title: "Real estate broker",
-    image: "./agents/agent-4.jpeg",
-    description:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam a velit fermentum, ultrices ligula sed, tincidunt est. Nullam vulputate sollicitudin nulla non pulvinar. In eget imperdiet magna.",
-  },
+  // {
+  //   name: "Levon Tomson",
+  //   title: "Real estate agent",
+  //   image: "./agents/agent-3.jpeg",
+  //   description:
+  //     "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam a velit fermentum, ultrices ligula sed, tincidunt est. Nullam vulputate sollicitudin nulla non pulvinar. In eget imperdiet magna.",
+  // },
+  // {
+  //   name: "Linnet Cubin",
+  //   title: "Real estate agent",
+  //   image: "./agents/agent-2.jpeg",
+  //   description:
+  //     "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam a velit fermentum, ultrices ligula sed, tincidunt est. Nullam vulputate sollicitudin nulla non pulvinar. In eget imperdiet magna.",
+  // },
+  // {
+  //   name: "Roddy Epsley",
+  //   title: "Real estate broker",
+  //   image: "./agents/agent-4.jpeg",
+  //   description:
+  //     "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam a velit fermentum, ultrices ligula sed, tincidunt est. Nullam vulputate sollicitudin nulla non pulvinar. In eget imperdiet magna.",
+  // },
 ];
